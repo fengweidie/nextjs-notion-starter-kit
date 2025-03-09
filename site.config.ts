9e6ab52fd882efafcc99988ae4a78ca691f4799e
fengweidie <69009489+fengweidie@users.noreply.github.com>
2025-03-09 08:12:43 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'fuli',
+  domain: 'rainj',
+  author: 'hujian',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'fuli',
 
   // social usernames (optional)
   twitter: 'transitive_bs',

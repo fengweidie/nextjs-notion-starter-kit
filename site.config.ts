a18bb1a77ce245bd47912fe0c712dbae830a69e6
fengweidie <69009489+fengweidie@users.noreply.github.com>
2025-03-09 09:59:17 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,7 @@ export default siteConfig({
   description: 'fuli',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
